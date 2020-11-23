f966d79b7c2969a30839270719c38d613be17653
xlong <381612175@qq.com>
2020-11-23 16:52:03 +0800

    新增样式

--- src/views/account-manage/index.vue
+++ src/views/account-manage/index.vue
@@ -31,8 +31,8 @@
       @change="handleTableChange"
     >
       <template #provinceAndCity="{ record }">
-        <span>{{ record.province }}</span
-        ><span v-if="record.city">/ {{ record.city }}</span>
+        <span>{{ record.province }}</span>
+        <span v-if="record.city">/ {{ record.city }}</span>
       </template>
       <template #action="{ record }">
         <a @click="openAccountStoreMoadl(record, 2)">
@@ -203,7 +203,198 @@ export default {
 </script>
 
 <style lang="less" scoped>
+@height: calc(100vh - 270px);
 .account-list-container {
   padding: 20px 0;
+}
+
+.account-detail {
+  &-container {
+    height: @height;
+    display: flex;
+  }
+
+  &__left {
+    min-width: 120px;
+    position: relative;
+
+    &:after {
+      content: "";
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      width: 2px;
+      height: 100%;
+      background-color: #dfe4ed;
+      z-index: 1;
+    }
+  }
+
+  &__right {
+    width: 100%;
+  }
+
+  &-tab {
+    min-width: 120px;
+    /deep/ .el-tabs__nav {
+      height: @height;
+      min-width: 120px;
+    }
+    /deep/ .el-tabs__item.is-left {
+      text-align: center;
+    }
+
+    /deep/ .el-tabs__item.is-active {
+      background-color: #def7f3;
+    }
+
+    /deep/ .el-tabs__header.is-left {
+      margin-right: 15px;
+    }
+
+    &__content {
+      height: @height;
+      padding-right: 10px;
+      overflow-y: auto;
+    }
+  }
+
+  &-content {
+    height: @height;
+    overflow-y: auto;
+    padding-right: 10px;
+
+    &__title {
+      font-size: 16px;
+      margin-bottom: 10px;
+      font-weight: bold;
+      // color: $color-primary;
+    }
+
+    &__cont {
+      margin-bottom: 20px;
+    }
+  }
+}
+
+@border: 1px solid #ebeef5;
+.info-table {
+  width: 100%;
+  border-top: @border;
+  border-left: @border;
+
+  &-container {
+    height: 100%;
+    min-height: 400px;
+  }
+
+  &__tr {
+    display: flex;
+  }
+
+  &__th,
+  &__td {
+    padding: 12px 10px;
+    border-bottom: @border;
+    border-right: @border;
+  }
+
+  &__td {
+    padding-bottom: 0;
+  }
+
+  &__th {
+    display: flex;
+    align-items: center;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    background: #fafafa;
+    width: 135px;
+    min-width: 135px;
+    max-width: 135px;
+  }
+
+  &__td {
+    width: 100%;
+  }
+
+  &__icon {
+    font-size: 20px;
+  }
+}
+
+.color-green {
+  color: @green;
+}
+
+.color-red {
+  color: @red;
+}
+
+.head-pic {
+  text-align: center;
+  display: flex;
+  align-items: center;
+  height: 100%;
+
+  &:hover {
+    .img-shade-actions {
+      opacity: 1;
+    }
+  }
+
+  img {
+    min-width: 100px;
+    max-width: 100px;
+    max-height: 138px;
+  }
+}
+
+.align-item-center {
+  display: flex;
+  align-items: center;
+}
+
+.img-shade {
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  &-thumbnail {
+    width: 100%;
+    height: 100%;
+    // cursor: pointer;
+  }
+
+  &-actions {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    cursor: default;
+    color: #fff;
+    opacity: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    transition: opacity 0.3s;
+
+    &-btn {
+      cursor: pointer;
+    }
+
+    /deep/ .el-upload:focus {
+      color: #fff;
+    }
+  }
+}
+
+.required::before {
+  content: "*";
+  color: @pink;
+  margin-right: 5px;
 }
 </style>
