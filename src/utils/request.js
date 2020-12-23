8d4e75ffd1c4ae5c1e4c32a5385825de33636c53
381612175@qq.com <381612175@qq.com>
2020-12-23 17:13:46 +0800

    添加webpack依赖，解决运行时“TypeError: compilation.templatesPlugin is not a function”报错

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,88 +1,88 @@
-import axios from 'axios'
-import { Message } from 'element-ui'
-import store from '@/store'
+import axios from "axios";
+import { Message } from "element-ui";
+import store from "@/store";
 
 export function handelRequestError(error) {
   switch (error.response.status) {
     case 401:
       Message({
-        message: '未授权！请重新登录',
-        type: 'error',
+        message: "未授权！请重新登录",
+        type: "error",
         duration: 5 * 1000
-      })
+      });
       setTimeout(() => {
-        store.dispatch('user/logout')
-      }, 500)
-      break
+        store.dispatch("user/logout");
+      }, 500);
+      break;
     case 403:
       Message({
         message: error.response.content,
-        type: 'error',
+        type: "error",
         duration: 5 * 1000
-      })
-      break
+      });
+      break;
     case 500:
       Message({
-        message: '系统繁忙',
-        type: 'error',
+        message: "系统繁忙",
+        type: "error",
         duration: 5 * 1000
-      })
-      break
+      });
+      break;
     default:
-      break
+      break;
   }
 }
 
 export function configSetting(config) {
-  config.headers['Content-Type'] = 'application/json'
+  config.headers["Content-Type"] = "application/json";
 
   if (store.getters.token) {
-    config.headers['Authorization'] = 'Bearer ' + store.getters.token
+    config.headers["Authorization"] = "Bearer " + store.getters.token;
   }
-  return config
+  return config;
 }
 
 const service = axios.create({
-  baseURL: '', // url = base url + request url
+  baseURL: "", // url = base url + request url
   timeout: 30000
-})
+});
 
 service.interceptors.request.use(
   config => {
-    return configSetting(config)
+    return configSetting(config);
   },
   error => {
-    console.log(error)
-    return Promise.reject(error)
+    console.log(error);
+    return Promise.reject(error);
   }
-)
+);
 
 service.interceptors.response.use(
   response => {
-    const res = response.data
+    const res = response.data;
 
     if (res.status) {
       if (res.status.code === 0) {
         Message({
-          message: res.status.text || 'Error',
-          type: 'error',
+          message: res.status.text || "Error",
+          type: "error",
           duration: 5 * 1000
-        })
+        });
 
-        return Promise.reject(new Error(res.status.text || 'Error'))
+        return Promise.reject(new Error(res.status.text || "Error"));
       } else {
-        return res
+        return res;
       }
     }
 
-    return res.custom
+    return res.custom;
   },
   error => {
     if (error && error.response) {
-      handelRequestError(error)
+      handelRequestError(error);
     }
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
-export default service
+export default service;
