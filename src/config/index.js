7d2bef33336a95494e3239c984d977c50a776621
381612175@qq.com <381612175@qq.com>
2021-08-10 10:27:53 +0800

    还原：hydrogen-js-sdk版本至2.2.4

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-export const APPLICATION_ID = "07ffab130cdf11f91381f18779fcf697";
-export const REST_API_KEY = "653887187daf660941971acf257cccc1";
-// export const SERCRET_KEY = "";
-// export const SECURITY_CODE = "";
+export const APPLICATION_ID = "";
+export const REST_API_KEY = "";
+export const SERCRET_KEY = "910eed9d6eea3983";
+export const SECURITY_CODE = "XL1029";
