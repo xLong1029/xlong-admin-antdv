d5455cc7fb03787fb4db726cd313356a5f3ec57c
xLong <381612175@qq.com>
2022-05-10 21:34:16 +0800

    antdvV2.2.8有坑，导航会出错，老子找了好久的BUG，暂时先降版本吧...

--- src/layout/default/components/NavBar/NavItem.vue
+++ src/layout/default/components/NavBar/NavItem.vue
@@ -1,43 +1,5 @@
 <template>
-  <a-menu-item
-    v-if="menuComponent === 'MenuItem'"
-    :key="currentItem.name"
-    @click.capture="handleLink(resolvePath(currentItem.path))"
-  >
-    <template v-if="currentItem.meta">
-      <i
-        v-if="currentItem.meta.icon"
-        :class="currentItem.meta.icon"
-        class="mr-5"
-      ></i>
-      <span>{{ currentItem.meta.title }}</span>
-    </template>
-  </a-menu-item>
-
-  <a-sub-menu
-    v-if="menuComponent === 'Submenu'"
-    :key="currentItem.name"
-    @click.capture="handleLink(resolvePath(currentItem.path))"
-  >
-    <template #title>
-
-        <i
-          v-if="currentItem.meta.icon"
-          :class="currentItem.meta.icon"
-          class="mr-5"
-        ></i>
-        <span>{{ currentItem.meta.title }}</span>
-      <!-- </template> -->
-    </template>
-      <NavItem
-      v-for="route in currentItem.children"
-      :key="route.path"
-      :item="route"
-      :base-path="resolvePath(route.path)"
-    />
-  </a-sub-menu>
-
-  <!-- <component
+  <component
     v-if="!currentItem.hidden"
     :is="menuComponent"
     :item="currentItem"
@@ -51,7 +13,7 @@
         :base-path="resolvePath(route.path)"
       />
     </template>
-  </component> -->
+  </component>
 </template>
 
 
@@ -62,7 +24,6 @@ import path from "path";
 import { isExternal } from "@/utils/validate";
 import MenuItem from "./MenuItem.vue";
 import Submenu from "./Submenu.vue";
-import { useRouter } from "vue-router";
 
 // eslint-disable-next-line
 const props = defineProps({
@@ -76,17 +37,7 @@ const props = defineProps({
   },
 });
 
-const router = useRouter();
-
-const handleLink = (url) => {
-  if (isExternal(url)) {
-    window.open(url);
-  } else {
-    router.push({ path: url });
-  }
-};
-
-const menuComponent = ref("MenuItem");
+const menuComponent = shallowRef(MenuItem);
 const currentItem = ref({
   hidden: true,
 });
@@ -101,23 +52,15 @@ const resolvePath = (routePath) => {
   return path.resolve(props.basePath, routePath);
 };
 
-const isHidden = (item) => {
-  // console.log(item.name, Object.keys(item).includes("hidden"));
-
-  return Object.keys(item).includes("hidden") && item.hidden;
-};
-
 // 通过判断获取当前菜单组件
 const getComponent = (children = [], parent) => {
   if (!children || !children.length) {
     currentItem.value = { ...parent, path: "", noShowingChildren: true };
-    return "MenuItem";
+    return MenuItem;
   }
 
-  console.log(children);
-
   const showingChildren = children.filter((item) => {
-    if (isHidden(item)) {
+    if (item.hidden) {
       return false;
     } else {
       currentItem.value = item;
@@ -125,20 +68,18 @@ const getComponent = (children = [], parent) => {
     }
   });
 
-  console.log(222, showingChildren, showingChildren.length, currentItem.value);
-
   if (showingChildren.length === 1) {
-    return "MenuItem";
+    return MenuItem;
   }
 
   if (showingChildren.length === 0) {
     currentItem.value = { ...parent, noShowingChildren: true };
     // console.log(333, currentItem.value);
-    return "MenuItem";
+    return MenuItem;
   }
   currentItem.value = { ...parent };
 
-  return "Submenu";
+  return Submenu;
 };
 
 onMounted(() => {
